b7e0f0fba2c59afe8ad9953453e2368ab79073ec
Mimemat <hyatomimemat@gmail.com>
2021-02-26 21:11:20 -0300

    Days 3 and 4

---
+++ lib/rocketpay/accounts/deposit.ex
@@ -0,0 +1,16 @@
+defmodule Rocketpay.Accounts.Deposit do
+  alias Rocketpay.{Repo, Accounts.Operation}
+
+  def call(params) do
+    params
+    |> Operation.call(:deposit)
+    |> run_transaction()
+  end
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, _op, reason, _changes} -> {:error, reason}
+      {:ok, %{account_deposit: account}} -> {:ok, account}
+    end
+  end
+end
---
+++ lib/rocketpay/accounts/withdraw.ex
@@ -0,0 +1,16 @@
+defmodule Rocketpay.Accounts.Withdraw do
+  alias Rocketpay.{Repo, Accounts.Operation}
+
+  def call(params) do
+    params
+    |> Operation.call(:withdraw)
+    |> run_transaction()
+  end
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, _op, reason, _changes} -> {:error, reason}
+      {:ok, %{account_withdraw: account}} -> {:ok, account}
+    end
+  end
+end
